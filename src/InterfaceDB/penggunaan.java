480af48d2c72298d9e76787eb535bf2711405fe3
andrianprasetya <andrianprasetya223@gmail.com>
2019-12-09 22:08:24 +0700

    First Push

---
+++ src/InterfaceDB/penggunaan.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InterfaceDB;
+
+/**
+ *
+ * @author Civilition
+ */
+public class penggunaan {
+        private String bulan;
+        private String tahun;
+        private String meter_awal;
+        private String meter_akhir;
+
+    public String getBulan() {
+        return bulan;
+    }
+
+    public void setBulan(String bulan) {
+        this.bulan = bulan;
+    }
+
+    public String getTahun() {
+        return tahun;
+    }
+
+    public void setTahun(String tahun) {
+        this.tahun = tahun;
+    }
+
+    public String getMeter_awal() {
+        return meter_awal;
+    }
+
+    public void setMeter_awal(String meter_awal) {
+        this.meter_awal = meter_awal;
+    }
+
+    public String getMeter_akhir() {
+        return meter_akhir;
+    }
+
+    public void setMeter_akhir(String meter_akhir) {
+        this.meter_akhir = meter_akhir;
+    }
+
+        
+}
